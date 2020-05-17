2863cfa5128c070c3ccbbd0b13df1f6565736f3a
user <sajjad@host.docker.internal>
2020-05-17 14:58:26 -0700

    added Hotel.java and HotelDemo.java in src folder with few suggestions

---
+++ src/hotelSystem/HotelDemo.java
@@ -0,0 +1,15 @@
+package hotelSystem;
+
+public class HotelDemo {
+
+	public static void main(String[] args) {
+//		int[] hotelRooms = new int[] { 1, 2 };
+//		Hotel hotel = new Hotel(hotelRooms);
+//		
+//		Integer[] days = new Integer[] { 1, 2};
+//		hotel.bookRoom("B-1", days, 1);
+//		
+//		
+	}
+
+}
